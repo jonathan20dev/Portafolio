c3742b510b217ab56e0fcc20d73278c0f6fdf47d
jonathan20dev <jonathan20.dev@gmail.com>
2022-10-06 02:59:39 -0600

    Services

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import './App.css';
-import {Header} from "./components/header/Header"
-import {Home} from "./components/home/Home"
-import {About} from "./components/about/About"
-import {Skills} from "./components/skills/Skills"
+import { Header } from "./components/header/Header"
+import { Home } from "./components/home/Home"
+import { About } from "./components/about/About"
+import { Skills } from "./components/skills/Skills"
+import {Services} from "./components/services/Services"
 
 const App = () => {
   return (
     <React.Fragment>
-    <Header/>
-    <main className="main">
-    <Home/>
-    <About/>
-    <Skills/>
-    </main>
+      <Header />
+      <main className="main">
+        <Home />
+        <About />
+        <Skills />
+        <Services/>
+      </main>
     </React.Fragment>
   )
 }
--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -1,45 +1,100 @@
 import React from 'react'
 
 const Backend = () => {
-  return (
-    <div className="skills__content">
-        <h3 className="skills__title"></h3>
+    return (
+        <div className="skills__content">
+            <h3 className="skills__title">Backend Technologies</h3>
 
-        <div className="skills__box">
-            <div className="skills__group">
+            <div className="skills__box">
+                <div className="skills__group">
 
-                <div className="skills__data">
-                    <i className="bx bx-badge-check"></i>
-                    <div>
-                        <h3 className="skills__name"></h3>
-                        <span className="skills__level"></span>
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">NodeJS</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
                     </div>
-                
+
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">SQL Server</h3>
+                            <span className="skills__level">Medium</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">MongoDB</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">APIREST</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">Docker</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+                    </div>
                 </div>
 
-                <div className="skills__data">
-                    <i className="bx bx-badge-check"></i>
-                    <div>
-                        <h3 className="skills__name"></h3>
-                        <span className="skills__level"></span>
-                    </div>
                 
-                </div>
+                <div className="skills__group">
 
-                <div className="skills__data">
-                    <i className="bx bx-badge-check"></i>
-                    <div>
-                        <h3 className="skills__name"></h3>
-                        <span className="skills__level"></span>
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">NodeJS</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
                     </div>
-                
+
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">SQL Server</h3>
+                            <span className="skills__level">Medium</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">MongoDB</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">APIREST</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">Docker</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+                    </div>
                 </div>
-
-
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
-export {Backend}
+export { Backend }
