5cac06738e679e092cba46a7c0b0ad139b0b5047
jonathan20dev <jonathan20.dev@gmail.com>
2022-10-05 22:00:28 -0600

    About me

--- src/components/home/ScrollDown.jsx
+++ src/components/home/ScrollDown.jsx
@@ -3,14 +3,12 @@ import React from 'react'
 const ScrollDown = () => {
   return (
     <div className="home__scroll">
-        <a href="about" className="home__scroll-button button--flex">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3" />
-</svg>
-
-        </a>
+      <a href="#about" className="home__scroll-button button--flex">
+         <span className="home__scroll-name">Scroll Down</span>
+         <i className="uil uil-arrow-down"></i>
+      </a>
     </div>
   )
 }
 
-export {ScrollDown}
+export { ScrollDown }
