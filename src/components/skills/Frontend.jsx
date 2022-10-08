c77a7582933216b6d2078ce34bfb197f67251883
jonathan20dev <jonathan20.dev@gmail.com>
2022-10-07 20:25:45 -0600

    Qualification V2

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -27,7 +27,7 @@ const Frontend = () => {
                     <div className="skills__data">
                         <i className="bx bx-badge-check"></i>
                         <div>
-                            <h3 className="skills__name">Tailwind</h3>
+                            <h3 className="skills__name">Figma</h3>
                             <span className="skills__level">Intermediate</span>
                         </div>
                     </div>
